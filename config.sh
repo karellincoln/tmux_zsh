617b532582f8f132d0072f36d7e221b10ce575ee
karellincoln <2218374291@qq.com>
2017-12-15 09:31:02 +0800

    refine tmux start and tmux.conf and fix some bug

--- config.sh
+++ config.sh
@@ -5,5 +5,7 @@ yum install -y zsh tmux
 mkdir -p ~/.oh-my-zsh
 /bin/cp -rf oh-my-zsh/* ~/.oh-my-zsh/
 /bin/cp -f oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /usr/bin/zsh
+
 
 /bin/cp -f tmux.conf ~/.tmux.conf
